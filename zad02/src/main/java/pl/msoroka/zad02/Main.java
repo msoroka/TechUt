ef171508bf5f53373d003ba443402e667c1156a3
Mateusz Soroka <xmsoroka@gmail.com>
2018-10-17 20:07:57 +0200

    Code refactor

--- zad02/src/main/java/pl/msoroka/zad02/Main.java
+++ zad02/src/main/java/pl/msoroka/zad02/Main.java
@@ -7,20 +7,11 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Plane plane1 = new Plane();
-        Plane plane2 = new Plane();
-
-        plane1.setProducer("Boeing 730");
-        plane1.setCapacity(375);
         // Date below is 2018-05-30
-        plane1.setProduceDate(new Date(118, 05, 30));
-        plane1.setCombustion(135.5);
+        Plane plane1 = new Plane("Boeing 730", 375, new Date(118, 05, 30), 135.5);
 
-        plane2.setProducer("Airbus A320");
-        plane2.setCapacity(220);
         // Date below is 1989-02-15
-        plane2.setProduceDate(new Date(89, 01, 15));
-        plane2.setCombustion(95.5);
+        Plane plane2 = new Plane("Airbus A320", 375, new Date(89, 01, 15), 95.5);
 
         PlaneService ps = new PlaneService();
 
